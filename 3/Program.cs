5fc6cbc981bedd450ee4da0fa09e123e705a39d1
Emanuel Ramírez <er6348100@gmail.com>
2026-04-08 23:18:04 -0600

    Fin ej 3

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿int cantidad = 0;
+int suma = 0;
+
+Console.WriteLine("Ingrese cuantos números quiere ingresar: ");
+Console.WriteLine();
+while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Presione ENTER para continuar");
+Console.WriteLine("");
+Console.ReadLine();
+Console.Clear();
+
+int[] numeros = new int[cantidad];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Ingrese el {i + 1}° números");
+    Console.WriteLine();
+    while (!int.TryParse(Console.ReadLine(), out numeros[i]))
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Dato no válido");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine("");
+    Console.ReadLine();
+    Console.Clear();
+    suma += numeros[i];
+}
+
+Console.WriteLine("La suma de los números ingresados es: ");
+Console.WriteLine();
+Console.WriteLine($"{suma}");
+
+
