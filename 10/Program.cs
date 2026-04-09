051447c0426830e4c05687e90d609a943cf2f5ad
Emanuel Ramírez <er6348100@gmail.com>
2026-04-09 15:14:27 -0600

    Fin ej 10

---
+++ 10/Program.cs
@@ -0,0 +1,66 @@
+﻿int cantidad = 0;
+double numerobuscar = 0;
+int repeticiones = 0;
+
+Console.WriteLine("Ingrese cuantos números quiere ingresar: ");
+Console.WriteLine();
+while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Presione ENTER para continuar");
+Console.WriteLine("");
+Console.ReadLine();
+Console.Clear();
+
+double[] numeros = new double[cantidad];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Ingrese el {i + 1}° números");
+    Console.WriteLine();
+    while (!double.TryParse(Console.ReadLine(), out numeros[i]))
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Dato no válido");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine("");
+    Console.ReadLine();
+    Console.Clear();
+}
+
+Console.WriteLine("Ingrese el número que quiere saber cuantas veces se repite: ");
+Console.WriteLine();
+while (!double.TryParse(Console.ReadLine(), out numerobuscar))
+{
+    Console.WriteLine("");
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine("Presione ENTER para continuar");
+Console.WriteLine("");
+Console.ReadLine();
+Console.Clear();
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (numeros[i] == numerobuscar)
+    {
+        repeticiones++;
+    }
+}
+
+Console.WriteLine($"El numero de veces que se repite {numerobuscar} es {repeticiones}");
+
+
+
+
+
