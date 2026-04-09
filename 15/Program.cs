fc4fa2b04d474cf20c1edf61d4fbb3d1c2a85687
Emanuel Ramírez <er6348100@gmail.com>
2026-04-09 16:34:06 -0600

    Fin ej 15

--- 15/Program.cs
+++ 15/Program.cs
@@ -1,1 +1,52 @@
-﻿
+﻿int cantidad = 0;
+double orden = 0;
+double valor = 0;
+
+Console.WriteLine("Ingrese cuantos números quiere ingresar: ");
+Console.WriteLine();
+while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Presione ENTER para continuar");
+Console.WriteLine("");
+Console.ReadLine();
+Console.Clear();
+
+double[] numeros = new double[cantidad];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Ingrese el {i + 1}° números");
+    Console.WriteLine();
+    while (!double.TryParse(Console.ReadLine(), out numeros[i]))
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Dato no válido");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine("");
+    Console.ReadLine();
+    Console.Clear();
+}
+
+Console.WriteLine("Los número ingresados mostrando si son pares o impares:");
+Console.WriteLine();
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        Console.Write($"         El número {numeros[i]} es par           ");
+        Console.Write("");
+    }
+    else
+    {
+        Console.Write($"         El número {numeros[i]} es impar           ");
+        Console.Write("");
+    }
+}
