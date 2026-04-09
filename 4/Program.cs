b7426a0e848638bd388137b73fe55e04e99b8bce
Emanuel Ramírez <er6348100@gmail.com>
2026-04-08 23:20:02 -0600

    Fin ej 4

---
+++ 4/Program.cs
@@ -0,0 +1,41 @@
+﻿int cantidad = 0;
+double suma = 0;
+
+Console.WriteLine("Ingrese cuantos números quiere ingresar: ");
+Console.WriteLine();
+while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Presione ENTER para continuar");
+Console.WriteLine("");
+Console.ReadLine();
+Console.Clear();
+
+double[] numeros = new double[cantidad];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Ingrese el {i + 1}° números");
+    Console.WriteLine();
+    while (!double.TryParse(Console.ReadLine(), out numeros[i]))
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Dato no válido");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine("");
+    Console.ReadLine();
+    Console.Clear();
+    suma += numeros[i];
+}
+
+Console.WriteLine("El promedio de los números ingresados es: ");
+Console.WriteLine();
+Console.WriteLine($"{suma/numeros.Length}");
+
