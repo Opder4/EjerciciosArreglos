1edb8cdba5344ccb8dcff8e81347e56d7364410d
Emanuel Ramírez <er6348100@gmail.com>
2026-04-09 15:38:47 -0600

    Fin ej 12

--- 12/Program.cs
+++ 12/Program.cs
@@ -1,6 +1,5 @@
 ﻿int cantidad = 0;
 
-
 Console.WriteLine("Ingrese cuantos números quiere ingresar: ");
 Console.WriteLine();
 while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
@@ -16,12 +15,13 @@ Console.ReadLine();
 Console.Clear();
 
 double[] numeros = new double[cantidad];
+double[] copia = new double[cantidad];
 
 for (int i = 0; i < numeros.Length; i++)
 {
     Console.WriteLine($"Ingrese el {i + 1}° números");
     Console.WriteLine();
-    while (!double.TryParse(Console.ReadLine(), out numeros[i]))
+    while (!double.TryParse(Console.ReadLine(), out numeros[i]) && !double.TryParse(Console.ReadLine(), out numeros[i]))
     {
         Console.WriteLine("");
         Console.WriteLine("Dato no válido");
@@ -34,21 +34,10 @@ for (int i = 0; i < numeros.Length; i++)
     Console.Clear();
 }
 
-Console.WriteLine("Los valores positivos son: ");
-Console.WriteLine();
-for (int i = 0; i < numeros.Length; i++)
-{
-    if (numeros[i]>0 )
-    {
-        Console.WriteLine($"{numeros[i]}");
-        Console.WriteLine();
-    }
-    else if (numeros[i]==0)
-    {
-        Console.WriteLine("El número 0 no es ni positivo ni negativo");
-        Console.WriteLine();
-    }
-}
+Console.WriteLine("Se hizo correctamente la Copia");
+
+//posdata: no entendí este ejercicio.
+
 
 
 
