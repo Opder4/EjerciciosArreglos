57d8bb93f9425e3deb202576f39887edd39e70f5
Emanuel Ramírez <er6348100@gmail.com>
2026-04-08 23:34:16 -0600

    Fin ej 7

---
+++ 7/Program.cs
@@ -0,0 +1,48 @@
+﻿int cantidad = 0;
+int contadorspares = 0;
+
+Console.WriteLine("Ingrese cuantos números quiere ingresar: ");
+Console.WriteLine();
+while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Presione ENTER para continuar");
+Console.WriteLine("");
+Console.ReadLine();
+Console.Clear();
+
+double[] numeros = new double[cantidad];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Ingrese el {i + 1}° números");
+    Console.WriteLine();
+    while (!double.TryParse(Console.ReadLine(), out numeros[i]))
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Dato no válido");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine("");
+    Console.ReadLine();
+    Console.Clear();
+}
+
+for (int i = 0; i < numeros.Length; i++)
+{
+   if (numeros[i]%2==0)
+    {
+        contadorspares++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Hay {contadorspares} números pares");
+
+
+
